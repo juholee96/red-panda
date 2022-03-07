76ee60d8ddfcadfcfe9c52515ec52578e8c747a8
Kevin Lee <juho.lee96@gmail.com>
2022-03-07 12:46:59 -0600

    Backend: Java classes

---
+++ server/src/main/java/Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    String itemName;
+    int amount;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,31 +1,53 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.*;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //This is required to allow GET and POST requests with the header 'content-type'
+        HashMap<String, Distributors> lowestPrices = getLowestPrices();
+        HashMap<String, Candy> testMapLow = getLowStockItems();
+        Collection<Candy> candyValues = testMapLow.values();
+        List<Candy> candies = new ArrayList<>(candyValues);
+
+        // This is required to allow GET and POST requests with the header
+        // 'content-type'
         options("/*",
                 (request, response) -> {
-                        response.header("Access-Control-Allow-Headers",
-                                "content-type");
+                    response.header("Access-Control-Allow-Headers",
+                            "content-type");
 
-                        response.header("Access-Control-Allow-Methods",
-                                "GET, POST");
-
+                    response.header("Access-Control-Allow-Methods",
+                            "GET, POST");
 
                     return "OK";
                 });
 
-        //This is required to allow the React app to communicate with this API
+        // This is required to allow the React app to communicate with this API
         before((request, response) -> response.header("Access-Control-Allow-Origin", "http://localhost:3000"));
 
-        //TODO: Return JSON containing the candies for which the stock is less than 25% of it's capacity
+        // TODO: Return JSON containing the candies for which the stock is less than 25%
+        // of it's capacity
         get("/low-stock", (request, response) -> {
-            return null;
-        });
+            return candies;
+        }, new JsonTransformer());
 
-        //TODO: Return JSON containing the total cost of restocking candy
+        // TODO: Return JSON containing the total cost of restocking candy
         post("/restock-cost", (request, response) -> {
             return null;
         });
